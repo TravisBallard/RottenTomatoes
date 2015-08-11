ed290a500b2a75464caaf4535417871ec17b35c9
Travis Ballard <travisballard@Ballard.local>
2015-08-11 00:38:25 -0700

    a little bit of styling

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	require_once( 'inc/lib/rotten.class.php' );
 	$rotten = new Rotten();
+
+	$x = 0;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,9 +10,12 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
+
 		<title>RottenTomatoes API Example</title>
 
-		<link rel="stylesheet" media="screen" href="inc/css/style.css" />
+		<link rel="stylesheet" media="screen" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,700,300|Montserrat:400,700">
+		<link rel="stylesheet" media="screen" type="text/css" href="inc/css/style.css" />
+
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="inc/js/script.js"></script>
@@ -22,16 +27,21 @@
 		</header>
 
 		<div class="container">
-			<?php foreach( $rotten->get_movies_in_box_office() as $movie ) { $movie = (object)$movie; ?>
-				<div class="movie">
+			<?php foreach( $rotten->get_movies_in_box_office() as $movie ) {
+				$movie = (object)$movie;
+				$classes = array( 'bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7' );
+				?>
+
+				<div class="movie <?php echo $classes[ $x % count( $classes ) ]; ?>">
 					<div class="poster">
 						<img src="<?php echo $movie->posters['profile']; ?>" alt="<?php printf( '%s (%d)', $movie->title, $movie->year ); ?>">
 					</div>
 					<div class="info">
 						<h2><?php echo $movie->title; ?></h2>
+						<p><?php echo $movie->synopsis; ?></p>
 					</div>
 				</div>
-			<?php } ?>
+			<?php $x++; } ?>
 		</div>
 
 		<footer></footer>
